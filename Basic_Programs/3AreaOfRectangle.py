8b37b68939fe8e43b4446f9164e2b7b7bcf05458
Vishal Rastogi <vishalrastogi1611@gmail.com>
2025-04-08 10:25:29 +0530

    Update 3AreaOfRectangle.py

---
+++ Basic_Programs/3AreaOfRectangle.py
@@ -0,0 +1,5 @@
+# 3.	Write a program to calculate the area of a rectangle.
+length = int( input("Enter Length of Rectangle : ") )
+width = int( input("Enter Width of Rectange : ") )
+area = length*width
+print("Area of Rectangle : ",area)
