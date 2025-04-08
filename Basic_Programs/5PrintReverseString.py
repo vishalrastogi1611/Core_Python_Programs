656435f852beb8cd6a208d56ce1a4467be5de018
Vishal Rastogi <vishalrastogi1611@gmail.com>
2025-04-08 10:27:23 +0530

    Update 5PrintReverseString.py

--- Basic_Programs/5PrintReverseString.py
+++ Basic_Programs/5PrintReverseString.py
@@ -1,2 +1,3 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# 5.	Write a Python program to take a string as input and print it in reverse order.
+string = input("Enter A String : ")
+print(string[::-1])
