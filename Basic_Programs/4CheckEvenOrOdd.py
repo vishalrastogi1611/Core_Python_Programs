18ea0c950fdfb5df15ef3ec9c9fbc397d18ac3a5
Vishal Rastogi <vishalrastogi1611@gmail.com>
2025-04-08 10:26:13 +0530

    Update 4CheckEvenOrOdd.py

--- Basic_Programs/4CheckEvenOrOdd.py
+++ Basic_Programs/4CheckEvenOrOdd.py
@@ -1,2 +1,6 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# 4.	Write a Python program to check if a number is even or odd.
+num = int( input("Enter A Number : ") )
+if(num%2==0):
+    print("Even Number")
+else:
+    print("Odd Number")
