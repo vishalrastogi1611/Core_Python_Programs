cb8ba875887ac9db8743b0419da9c7aba8410f16
Vishal Rastogi <vishalrastogi1611@gmail.com>
2025-04-08 10:27:53 +0530

    Update 6CelsiusToFahrenheit.py

--- Basic_Programs/6CelsiusToFahrenheit.py
+++ Basic_Programs/6CelsiusToFahrenheit.py
@@ -1,2 +1,4 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# 6.	Write a Python program to convert temperature from Celsius to Fahrenheit.
+celsius = float( input("Enter Temperature in Celsius : ") )
+fahrenheit = (celsius*9/5)+32
+print("Fahrenheit : ",fahrenheit)
