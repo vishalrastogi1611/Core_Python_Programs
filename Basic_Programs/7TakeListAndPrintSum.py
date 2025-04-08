8c650ec6603ca18ade4b07db340a2d99d69fa688
Vishal Rastogi <vishalrastogi1611@gmail.com>
2025-04-08 10:28:18 +0530

    Update 7TakeListAndPrintSum.py

--- Basic_Programs/7TakeListAndPrintSum.py
+++ Basic_Programs/7TakeListAndPrintSum.py
@@ -1,2 +1,6 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# 7.	Write a program to take a list of numbers and print their sum.
+list1 = [2,4,6,8,7]
+sum = 0
+for i in list1:
+    sum=sum+i
+print("Sum : ",sum)
